2e7f0d7f1df2a8bb081f528b3ec3e3c03c258e33
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-03 22:34:29 +0000

    refactor(api): restructure view templates

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d2442f1fe9e47eab9ab8cb1a0f6c33d = () => {
-  return '2025-03-20T15:37:13Z';
+const node_44a90835198d49f28b2fbb761cf3b055 = () => {
+  return '2025-04-03T22:34:29Z';
 };
