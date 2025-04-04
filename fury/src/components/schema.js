1263b5ff072daf81bba3b32eaad95b4b7ebe0b26
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 15:08:55 +0000

    feat(utils): sanitize view templates

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b941f16952847dca0d3510296effb13 = () => {
-  return '2025-03-20T19:39:47Z';
+const node_6bbfd66f68a7458eadd1d7b58ec446b3 = () => {
+  return '2025-04-04T15:08:55Z';
 };
