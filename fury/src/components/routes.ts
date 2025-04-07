29be425afbab474f81d1a595b7ba6447aa7d4c83
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 08:55:21 +0000

    fix(ui): restructure payload parser

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a59d6c9f53648859d0f96c2338cd717 = () => {
-  return '2025-04-03T23:37:33Z';
+const node_d4c7f8eb1d7848458762241918ae563c = () => {
+  return '2025-04-07T08:55:21Z';
 };
