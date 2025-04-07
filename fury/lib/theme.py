81ccebfcbc5fbb53640c37be3e9d05cdb65dfca6
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 10:28:29 +0000

    perf(core): sanitize logging interface

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf3caa1f69254df093f0242d147e18ed = () => {
-  return '2025-03-18T22:06:27Z';
+const node_7270e84c37364676a1c0d25ec3d60e2c = () => {
+  return '2025-04-07T10:28:29Z';
 };
