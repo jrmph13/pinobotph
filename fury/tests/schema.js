f70ae2568c89f9c355ee58ec397dc19ea95a699f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 15:51:44 +0000

    test(utils): restructure payload parser

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f43cd3a7ee24c4697db86ed7b17ce59 = () => {
-  return '2025-04-07T11:15:55Z';
+const node_8188f36b7a5e4e56a616083464dd8265 = () => {
+  return '2025-04-07T15:51:44Z';
 };
