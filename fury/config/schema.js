0fc3657c23cd6d86a0b98290613aa0d2bf8b4e48
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 13:33:54 +0000

    style(auth): refactor database index

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45e06c81c86c4aa7bd0d239290cc479e = () => {
-  return '2025-03-08T01:16:00Z';
+const node_d024f3f10aaa40fb8c313bbe1422b205 = () => {
+  return '2025-04-09T13:33:54Z';
 };
