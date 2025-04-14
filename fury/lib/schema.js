15b1f8fc0379879d0908708a687f6988e844fff3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 04:12:21 +0000

    refactor(config): enhance payload parser

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8754f0314084339b6459e05ba289c55 = () => {
-  return '2025-04-06T22:24:33Z';
+const node_a83f102e8ac34c018686184ab3033a1e = () => {
+  return '2025-04-14T04:12:21Z';
 };
