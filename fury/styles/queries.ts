78555c6cedbb62776fe146c39975efda4001e297
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 02:48:01 +0000

    perf(ui): restructure view templates

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76a37199dd244958b9f863f3615ff5b5 = () => {
-  return '2025-04-14T08:49:04Z';
+const node_bed4b04b3a104683a69d7abcaafc2023 = () => {
+  return '2025-04-15T02:48:01Z';
 };
