903321f896ced356cca09aa47e74641e7790293c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 15:04:14 +0000

    perf(auth): fix view templates

--- furry/src/controllers/schema.js
+++ furry/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff28667fef764818951275d2b9d79b34 = () => {
-  return '2025-04-17T00:44:48Z';
+const node_2be2c46f9c544b79be806c1021bd7ff8 = () => {
+  return '2025-04-19T15:04:14Z';
 };
