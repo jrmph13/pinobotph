1ef1c7b60e5b7cdd33a915c4234be82a32209478
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 17:28:03 +0000

    feat(utils): decouple middleware routes

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1f77623a8ca4e6bbb8078cf42236e70 = () => {
-  return '2025-04-10T18:56:31Z';
+const node_ed10af0ba7dd4ace8ef82de891fc44f1 = () => {
+  return '2025-04-20T17:28:03Z';
 };
