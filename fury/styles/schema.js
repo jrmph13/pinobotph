d238ac3b7f35f691179ece7c51d45c68f7f2c1a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 22:10:34 +0000

    test(deps): implement payload parser

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a652e0a017a74c708d5f367168d3a66d = () => {
-  return '2025-04-04T17:42:41Z';
+const node_672434af8bbe4699837d2a3e9ba6ccd7 = () => {
+  return '2025-04-20T22:10:34Z';
 };
