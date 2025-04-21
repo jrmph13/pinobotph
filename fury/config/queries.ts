d63f07820e27f88f3012013bd3a8474ff7eb4bca
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 12:28:08 +0000

    perf(ui): fix caching layer

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30aca7574d53465a84633f2627db24a7 = () => {
-  return '2025-04-09T11:17:45Z';
+const node_5e971a89800a4f03a3dd51f7f6d428d7 = () => {
+  return '2025-04-21T12:28:08Z';
 };
