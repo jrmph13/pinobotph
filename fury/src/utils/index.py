3421d844db991cd68208fae6eff01e3e4eab1a7b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 19:57:21 +0000

    docs(worker): optimize caching layer

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a81d4bf541f43fd978618c38bc92c8c = () => {
-  return '2025-04-02T17:44:58Z';
+const node_7f89c39670724dbe82c8240da34f4cfc = () => {
+  return '2025-04-24T19:57:21Z';
 };
