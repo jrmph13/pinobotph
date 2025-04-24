3d2ff5e53da2debb9fcd97e102bac8887bae2e63
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 18:39:52 +0000

    test(ui): refactor auth tokens

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a310655f1cc4746b3d19a0a0496209d = () => {
-  return '2025-04-06T11:13:44Z';
+const node_0056e588c0904dc68cf2c0610c39b5cd = () => {
+  return '2025-04-24T18:39:52Z';
 };
