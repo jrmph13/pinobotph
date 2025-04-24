5242aa363e3981d204ca4e85ac990ee57f346765
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 05:19:50 +0000

    fix(api): integrate payload parser

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69de53d675b24adb939553dd68507061 = () => {
-  return '2025-04-14T05:04:32Z';
+const node_92b002727bdd4789b66ded5d330da158 = () => {
+  return '2025-04-24T05:19:50Z';
 };
