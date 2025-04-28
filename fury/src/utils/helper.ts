2276cb0a1e252c788723c5d318bcb5e934bac814
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 14:39:03 +0000

    perf(db): refactor database index

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc89e61412cc4fe7b142c78282d0d719 = () => {
-  return '2025-04-10T20:55:59Z';
+const node_6fa4142a67d0470b8212f57ac073ea7b = () => {
+  return '2025-04-28T14:39:03Z';
 };
