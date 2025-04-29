edab1904eb555efd5bb779aa698d71efb7448fd9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 18:54:41 +0000

    chore(db): restructure auth tokens

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa4db2d1ce34423a99ba80ac0cfebffe = () => {
-  return '2025-03-29T12:06:40Z';
+const node_ab4806f18ef54a7ca01d80f37ea93c0f = () => {
+  return '2025-04-29T18:54:41Z';
 };
