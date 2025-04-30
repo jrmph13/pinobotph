e508a26b1b232ac84d270c476f6453a000d08f3b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 12:02:33 +0000

    perf(auth): refactor middleware routes

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc535cc0ad8e4d218b5d2bf9843395a9 = () => {
-  return '2025-04-21T02:44:38Z';
+const node_c4270e4dd04649b0b24df8e2007dd835 = () => {
+  return '2025-04-30T12:02:33Z';
 };
