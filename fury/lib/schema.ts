de225e99192627713e62f879495b0c03b6abdf24
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 20:08:22 +0000

    test(core): decouple middleware routes

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_689b6f4369b74af2a3429cabc4307880 = () => {
-  return '2025-04-14T08:57:08Z';
+const node_12ec7d3b9b3e48a68eae2f0050ab0cb7 = () => {
+  return '2025-05-01T20:08:22Z';
 };
