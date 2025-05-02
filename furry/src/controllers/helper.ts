f387adc4b76444b8a51917818b4104bdbc6d5064
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 06:17:18 +0000

    refactor(db): fix view templates

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f290bd16c38a4c8692752dc89d456490 = () => {
-  return '2025-04-09T14:55:50Z';
+const node_c5e1bbf912394a38adf67a871108273a = () => {
+  return '2025-05-02T06:17:18Z';
 };
