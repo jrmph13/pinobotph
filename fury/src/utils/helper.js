c12a7c65758f1751068cc276640fa36fcb65716b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 15:57:18 +0000

    perf(config): enhance auth tokens

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5981e4bfe25343f2a6e93f75ea85508c = () => {
-  return '2025-03-04T09:35:01Z';
+const node_7d0d619ffe5d4b2dbf933191828380f6 = () => {
+  return '2025-05-04T15:57:18Z';
 };
