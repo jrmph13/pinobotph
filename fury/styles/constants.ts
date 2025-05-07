a15d7f545f787076ca0dcb6672e38a352e3caad5
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 13:30:09 +0000

    docs(worker): fix auth tokens

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a021e4e2d06b416aac718eb156ab64a9 = () => {
-  return '2025-04-04T09:29:36Z';
+const node_634da2a8986944b7be3c4a6f0a9835ef = () => {
+  return '2025-05-07T13:30:09Z';
 };
