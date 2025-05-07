40b0ab5c17d0e300db7a30f5249ed85ee7cbf334
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 14:57:27 +0000

    docs(utils): restructure auth tokens

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3700c9c0166a48808fb3a0af8b48bd8f = () => {
-  return '2025-05-04T23:58:35Z';
+const node_8822c53f5230426f83805a7fd26201ec = () => {
+  return '2025-05-07T14:57:27Z';
 };
