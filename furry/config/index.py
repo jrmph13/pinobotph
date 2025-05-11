1755101f3aeeac753b35d71a532eedf7b5b15780
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 20:48:37 +0000

    refactor(config): sanitize middleware routes

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6b1720052604ab0a5d46ef18489ee06 = () => {
-  return '2025-03-10T22:59:11Z';
+const node_2bbd0a477989442d8c7a5dc764ed7967 = () => {
+  return '2025-05-11T20:48:37Z';
 };
