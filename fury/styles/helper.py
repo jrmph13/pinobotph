39b7fa0ca4c7570203a5882d70bf5961353fc7e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 10:43:38 +0000

    style(deps): fix view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f708511bdda4cf6a85234f20c051588 = () => {
-  return '2025-05-06T00:21:21Z';
+const node_6865da5982ff4c0bae77d17bab04aca4 = () => {
+  return '2025-05-13T10:43:38Z';
 };
