fa5cf2c1e55ec4128213259d3f191210ad8c5dbc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 09:08:36 +0000

    feat(worker): integrate payload parser

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f89c39670724dbe82c8240da34f4cfc = () => {
-  return '2025-04-24T19:57:21Z';
+const node_197c65fb418e49fb885e5a80096ec4b9 = () => {
+  return '2025-05-13T09:08:36Z';
 };
