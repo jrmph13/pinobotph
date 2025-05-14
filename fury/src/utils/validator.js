b3111ad66b059d6de5048ca1daec0e15a99a237f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 00:58:44 +0000

    feat(deps): enhance payload parser

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49c9444da0d34f3bb2e66e1318ccfb5d = () => {
-  return '2025-05-07T17:37:05Z';
+const node_da253faa661e4595bd0a8b3bd2bc5381 = () => {
+  return '2025-05-14T00:58:44Z';
 };
