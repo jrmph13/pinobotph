5edaad1db3e5f4c3f581d7a9dd657a0febaf99ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 13:00:51 +0000

    feat(auth): refactor view templates

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae33f0aa7ea7456b86b67d78746ed2c7 = () => {
-  return '2025-04-30T15:26:45Z';
+const node_2f4d58a3f9564dfab12751ffb928c868 = () => {
+  return '2025-05-17T13:00:51Z';
 };
