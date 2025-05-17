06648e52b790586c7889a4d28697b686a256c4bf
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 11:58:21 +0000

    feat(auth): implement validation pipeline

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b59b2da731840d28819964a9ab0cf2c = () => {
-  return '2025-05-15T19:54:08Z';
+const node_5f6478e91f4e4de6bf5d50c800fe42df = () => {
+  return '2025-05-17T11:58:21Z';
 };
