ba2f452b341ec2f8dbdd94068dcdd32a21dcd9d6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 03:51:55 +0000

    feat(ui): optimize payload parser

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f7c4e870edc49e597b9d85f22e7b2ee = () => {
-  return '2025-04-15T22:19:53Z';
+const node_14a09d1dc6be4bb3a3ae75529f017e19 = () => {
+  return '2025-05-18T03:51:55Z';
 };
