5011bd238f70e42d7f1a18b9a9b257d46bed8721
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 11:17:17 +0000

    perf(core): restructure database index

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21d4afc280114212b83aa45d7e321c2f = () => {
-  return '2025-05-01T02:35:37Z';
+const node_13cdb944d0254d93a657243ca32d38f9 = () => {
+  return '2025-05-19T11:17:17Z';
 };
