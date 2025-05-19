08277a532e36d9a16329602d4a8ded5285d11e27
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 02:22:35 +0000

    chore(ui): fix payload parser

--- furry/src/controllers/constants.ts
+++ furry/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e5639ffd1aa43afb72608aca9f803fb = () => {
-  return '2025-04-08T00:03:48Z';
+const node_d457110cf1ce467eaff386f40ed3c1c8 = () => {
+  return '2025-05-19T02:22:35Z';
 };
