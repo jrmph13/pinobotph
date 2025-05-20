7eb7711962e7c2ba1f86ab3da95c85df8b4591ff
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-20 01:06:44 +0000

    fix(utils): refactor view templates

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e971a89800a4f03a3dd51f7f6d428d7 = () => {
-  return '2025-04-21T12:28:08Z';
+const node_7a8c5c61495c45fabd035faf4d6be6aa = () => {
+  return '2025-05-20T01:06:44Z';
 };
