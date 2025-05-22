dd92929c94970497694c1eef20b0c43ef82761de
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 16:16:01 +0000

    fix(auth): optimize database index

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5fea13f466574a7cbdb2901f31917a54 = () => {
-  return '2025-04-18T00:17:19Z';
+const node_60174f9f9678459e9bf1bd9f974bfcb5 = () => {
+  return '2025-05-22T16:16:01Z';
 };
