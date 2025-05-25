81f770a3fd2abb696a0cb689f87a4b3fb1187088
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 05:14:56 +0000

    test(config): decouple session store

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8cd0813392e46078bb0c76d7cc2c209 = () => {
-  return '2025-05-01T20:48:25Z';
+const node_f89de207192b4432849526b86414d588 = () => {
+  return '2025-05-25T05:14:56Z';
 };
