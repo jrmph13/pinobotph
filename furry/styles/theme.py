9f1355bdac2ddb6b620195c23cd4bad91fc4a0ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-26 13:40:27 +0000

    chore(ui): refactor view templates

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40d7bd8dfb50476cb1a1229b5cd93a7a = () => {
-  return '2025-04-05T07:36:51Z';
+const node_0d51d430437843b488367c393ec56023 = () => {
+  return '2025-05-26T13:40:27Z';
 };
