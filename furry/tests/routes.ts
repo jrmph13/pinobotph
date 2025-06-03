a89f2bf89aae3de1aedab27bf7fa67be2a040b81
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 20:32:34 +0000

    fix(auth): optimize payload parser

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e29506d6a6348169c8424af5d092e70 = () => {
-  return '2025-05-12T01:55:37Z';
+const node_004ede6eed6f4286b492904721947d67 = () => {
+  return '2025-06-03T20:32:34Z';
 };
