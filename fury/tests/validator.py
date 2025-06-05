3173617a00da91c7e8cdfd7f52d28e9932fa5518
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-05 01:53:09 +0000

    fix(db): refactor view templates

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2df7f3356ab84bb19bece11557b45122 = () => {
-  return '2025-05-22T12:35:32Z';
+const node_cb1e74284d22424883fec6ad7ac83dd0 = () => {
+  return '2025-06-05T01:53:09Z';
 };
