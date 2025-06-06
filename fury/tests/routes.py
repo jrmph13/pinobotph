82203862eb89986e9dcdd0ff2102ff783af94da4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 00:15:13 +0000

    fix(auth): refactor view templates

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b75fa7ec78044b968c7b2551d5cc9708 = () => {
-  return '2025-05-30T21:18:57Z';
+const node_19359b5dda5a4a6fa845a9302dfda1e7 = () => {
+  return '2025-06-06T00:15:13Z';
 };
