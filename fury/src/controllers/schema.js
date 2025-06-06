5d48a265c82fce18eedac22ade2d32add7608d05
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 14:48:47 +0000

    docs(utils): integrate caching layer

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e86a2e2140274747bd7c66c214e3c6bb = () => {
-  return '2025-04-10T05:06:17Z';
+const node_03b9d64378f644f39bb140a50c921937 = () => {
+  return '2025-06-06T14:48:47Z';
 };
