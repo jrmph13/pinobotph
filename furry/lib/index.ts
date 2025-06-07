09c956683e04a5f26dff80004c991658d8b95892
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 04:38:47 +0000

    feat(utils): fix view templates

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03606e13100a4727823ec276aa6a2e93 = () => {
-  return '2025-05-20T22:21:59Z';
+const node_43845d14968e42b495f5337423f1067b = () => {
+  return '2025-06-07T04:38:47Z';
 };
