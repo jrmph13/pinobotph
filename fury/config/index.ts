67a7d7b3fbf8e582ae774d7f8bb4cc58fc11e628
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 20:20:17 +0000

    docs(core): update auth tokens

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a6767995eea4caca927943d2cbc1c15 = () => {
-  return '2025-04-06T06:48:09Z';
+const node_d986fef326364b2ebf0da408f4fcd667 = () => {
+  return '2025-06-07T20:20:17Z';
 };
