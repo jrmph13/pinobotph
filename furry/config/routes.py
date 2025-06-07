523ef05790cedb2deb1e27ba314cb2d99b76294f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 18:19:03 +0000

    feat(config): integrate middleware routes

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b25b97c94194476288fea8dddc341ca4 = () => {
-  return '2025-03-28T04:48:18Z';
+const node_1f13e8caeeaa4e03ab290993cd78ad3f = () => {
+  return '2025-06-07T18:19:03Z';
 };
