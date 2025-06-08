23d824fa64f7d91d45463a8fc6ce4ef7e7c13b64
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 14:41:11 +0000

    feat(ui): fix database index

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9818779f1d54ec8bc689ce67bd96a87 = () => {
-  return '2025-05-26T12:52:45Z';
+const node_34c1fb838cd44ed7bb34d3d57ab1f223 = () => {
+  return '2025-06-08T14:41:11Z';
 };
