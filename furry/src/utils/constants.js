8f64101931110454522f19bbdca26cb31257473c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 09:09:08 +0000

    feat(utils): decouple middleware routes

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0f6f070f3e04aa9b21e0e3a7142236b = () => {
-  return '2025-05-23T14:22:26Z';
+const node_4c11dfdab5884baab5da996806765445 = () => {
+  return '2025-06-08T09:09:08Z';
 };
