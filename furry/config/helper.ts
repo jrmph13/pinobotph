206a9d74ac87c311b2b980b60a7f055f340fc59a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 13:16:10 +0000

    feat(worker): restructure caching layer

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3ebe31efe1e499ba813ef58744fb898 = () => {
-  return '2025-05-13T13:38:00Z';
+const node_cc8e0b26bb9f4ca4be0c5a6e92c65483 = () => {
+  return '2025-06-08T13:16:10Z';
 };
