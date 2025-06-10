6745f2ed457e4b3ee70d259ba22d67d2078efa28
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 22:13:47 +0000

    feat(deps): sanitize database index

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc95cdee51f64d77a5b7f0f9a20709bc = () => {
-  return '2025-05-31T04:56:22Z';
+const node_061c08f11b7947839a7e89250341f6aa = () => {
+  return '2025-06-10T22:13:47Z';
 };
