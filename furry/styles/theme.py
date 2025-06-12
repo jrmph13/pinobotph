1484c32bb58e144633e2fc59dafd032152fe4dcd
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 02:29:23 +0000

    feat(worker): update view templates

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6a1b95d5692448fa6789891f4b6014e = () => {
-  return '2025-06-06T15:27:42Z';
+const node_4a11d872542a47e1bc6839df34dc17dc = () => {
+  return '2025-06-12T02:29:23Z';
 };
