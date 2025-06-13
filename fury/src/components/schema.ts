91cf7d041e1b6fa62a88621b8d3b1ee5e717b5ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 12:06:01 +0000

    feat(auth): fix session store

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_934457036e604b7ba6422dc5fbcc3eed = () => {
-  return '2025-06-07T23:07:13Z';
+const node_0a55c3df8c8148989a790313a387f8d5 = () => {
+  return '2025-06-13T12:06:01Z';
 };
