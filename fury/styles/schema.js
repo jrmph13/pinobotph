3fbd168d6ba9134cae301dc20ffecbd1e34af3b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 08:07:50 +0000

    fix(api): implement view templates

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db4b3cf952504a448be578bd2a166d6f = () => {
-  return '2025-05-21T12:30:44Z';
+const node_18d23d4e15d64cd39fc0e475bdebc64a = () => {
+  return '2025-06-13T08:07:50Z';
 };
