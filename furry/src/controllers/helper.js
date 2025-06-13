210b1e41f277ac8ffa5e0972edd707f55ae3ffed
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 21:48:34 +0000

    test(core): restructure payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_97cfd1419d514fce8c813ffc189d071b = () => {
-  return '2025-04-14T05:32:40Z';
+const node_57fdce5a97df49b885ec88dc30ab7351 = () => {
+  return '2025-06-13T21:48:34Z';
 };
