3fa44eecd498b6750f3c08c9546796aa82f6bea8
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 19:29:39 +0000

    feat(ui): enhance payload parser

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a55c3df8c8148989a790313a387f8d5 = () => {
-  return '2025-06-13T12:06:01Z';
+const node_30478eceeafe455e8869dff65f04cebc = () => {
+  return '2025-06-16T19:29:39Z';
 };
