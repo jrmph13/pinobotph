a55472d4f2a5f72b89c21c88348cce682e362c91
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 21:16:59 +0000

    chore(db): sanitize database index

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_259545ceb6b84b2c9c05645bec4863a2 = () => {
-  return '2025-04-30T09:18:15Z';
+const node_b0354052a93b46f1a8a83f972b8dd9e8 = () => {
+  return '2025-06-22T21:16:59Z';
 };
