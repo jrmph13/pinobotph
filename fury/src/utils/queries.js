6fb1431b2e5fedd9bbf32fb27a790a194dc79821
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 15:32:24 +0000

    chore(utils): restructure middleware routes

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c4c8b99773843359f58b893b8c12de9 = () => {
-  return '2025-06-21T06:35:58Z';
+const node_edc4c3815cbb4fbdad4b4a7d367cf983 = () => {
+  return '2025-06-22T15:32:24Z';
 };
