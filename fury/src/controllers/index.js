7dff87242c2bd5befb1c5781a0a7f2b7fe4eb8b6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 09:51:01 +0000

    chore(auth): update session store

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd13499b06d7458bb5661694ccbb1d71 = () => {
-  return '2025-03-04T10:59:32Z';
+const node_50eec4c1fce24645b74ffa82a29edb5d = () => {
+  return '2025-06-24T09:51:01Z';
 };
