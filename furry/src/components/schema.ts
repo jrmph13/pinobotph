594c2431607d0c6fa06a5d07c29f84c2b49e3475
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 05:10:52 +0000

    fix(api): enhance middleware routes

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_924c9b74f2e042849c97496d774e0477 = () => {
-  return '2025-06-18T14:40:12Z';
+const node_177f79ab40ab4a239ac291050905f954 = () => {
+  return '2025-06-25T05:10:52Z';
 };
