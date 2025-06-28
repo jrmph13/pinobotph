fe3a06a49688b63a4eed9905a9c0f9b003204af3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 09:23:16 +0000

    chore(deps): optimize session store

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_123253af4e584dd5bf4f7495bfa169ce = () => {
-  return '2025-04-07T00:38:18Z';
+const node_f7bf8db488f64a22b451a4fd93ac83f0 = () => {
+  return '2025-06-28T09:23:16Z';
 };
