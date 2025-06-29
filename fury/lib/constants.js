3686e8e304586d7dcd7f0c018fd00d232bf728a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 17:44:01 +0000

    style(core): refactor database index

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_511e882a238847eabcc045f119c5cb44 = () => {
-  return '2025-06-26T22:43:33Z';
+const node_3b8c3e26919b461884aacccc04c9a064 = () => {
+  return '2025-06-29T17:44:01Z';
 };
