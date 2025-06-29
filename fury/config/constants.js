01aa0076034329ce91d1449ec0d14997fe8706c3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 04:07:29 +0000

    refactor(deps): sanitize view templates

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_257b9955ff6041829f546192189fc125 = () => {
-  return '2025-05-17T13:06:43Z';
+const node_55f315573b8942f181b0535e2f199dfa = () => {
+  return '2025-06-29T04:07:29Z';
 };
