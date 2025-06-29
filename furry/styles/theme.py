82a42e8a7170c5e51243f42b76aa4abbf7ca05da
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 18:11:27 +0000

    fix(auth): restructure caching layer

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3062fb6eaaa4edcb607a17bf87ed22b = () => {
-  return '2025-06-25T12:03:16Z';
+const node_887ef5ced29846598b68e7076f2791db = () => {
+  return '2025-06-29T18:11:27Z';
 };
