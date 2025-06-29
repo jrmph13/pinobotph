82d2d7650cde42265b38074fd73b7e5e7429d979
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 14:04:23 +0000

    chore(config): update view templates

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b2ec5a21cb2c4446a0e8c64138a27098 = () => {
-  return '2025-06-03T16:25:16Z';
+const node_3004f32cc9a9438d93986d98df3d8f7d = () => {
+  return '2025-06-29T14:04:23Z';
 };
