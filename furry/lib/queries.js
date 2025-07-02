5daae36cf07c9dcb35a5127036eb4f200d232626
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 13:55:54 +0000

    docs(core): restructure caching layer

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c421561c5e94bc2ac3bd6891470004b = () => {
-  return '2025-05-26T23:55:23Z';
+const node_a2f9a1b4ad364e6c81edb3eaf76728a5 = () => {
+  return '2025-07-02T13:55:54Z';
 };
