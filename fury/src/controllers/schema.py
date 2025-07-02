3edb4db5992af22fe31a74f7d5380f7541e96909
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 08:39:50 +0000

    chore(config): update payload parser

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4270e4dd04649b0b24df8e2007dd835 = () => {
-  return '2025-04-30T12:02:33Z';
+const node_b826819850b648c2840e184c7b176126 = () => {
+  return '2025-07-02T08:39:50Z';
 };
