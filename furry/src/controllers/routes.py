354fa1ef7c44c86c42710c8133d882202a3ed950
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 02:19:24 +0000

    fix(auth): fix caching layer

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f82cac35cd3544b6a81ecff584fce802 = () => {
-  return '2025-05-24T20:27:32Z';
+const node_635c4fe73e454b18903f6ce277dfbbf3 = () => {
+  return '2025-07-02T02:19:24Z';
 };
