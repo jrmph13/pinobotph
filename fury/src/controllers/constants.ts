63740ab1b9f3e2cce29f0693c5345d4845c57ec2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 14:11:53 +0000

    perf(auth): sanitize view templates

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ffa7a9d4c9a43319b23ede0822d0b3f = () => {
-  return '2025-06-29T14:38:58Z';
+const node_60d73906891646f3aae745b7e98523cd = () => {
+  return '2025-07-02T14:11:53Z';
 };
