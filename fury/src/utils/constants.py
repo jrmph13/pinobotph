e309e3d32b7cb7f27137a6f54f258367fb2f3e05
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 11:04:49 +0000

    style(core): implement auth tokens

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e572dcf4cae943c1a19bf639be7fd606 = () => {
-  return '2025-05-28T22:25:17Z';
+const node_4f0d89cf453345debbebad635f604592 = () => {
+  return '2025-07-02T11:04:49Z';
 };
