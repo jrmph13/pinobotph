fc94fe73951e1118a9cdd29953ded31c5012303a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 01:44:22 +0000

    test(api): restructure auth tokens

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d2570bb6dad48deb6b935a2213deec8 = () => {
-  return '2025-05-18T13:03:24Z';
+const node_912e4e97ba554e3eb796e078672e626e = () => {
+  return '2025-07-02T01:44:22Z';
 };
