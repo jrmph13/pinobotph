cc856c7f13629697c4eca37051080a9b8e064b6f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 12:19:59 +0000

    fix(ui): refactor middleware routes

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4711654b16984051a7b100a6cf413357 = () => {
-  return '2025-06-24T18:00:23Z';
+const node_18f0f0e4c1794a3faef802b3e6aaa233 = () => {
+  return '2025-07-02T12:19:59Z';
 };
