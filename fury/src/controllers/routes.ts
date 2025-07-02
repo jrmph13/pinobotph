6a575b5b1fce712b707f7c071502c1e3d2ea87b9
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 22:57:32 +0000

    feat(utils): fix auth tokens

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34c1fb838cd44ed7bb34d3d57ab1f223 = () => {
-  return '2025-06-08T14:41:11Z';
+const node_cb99c45ddf134656938315f787e0c7cc = () => {
+  return '2025-07-02T22:57:32Z';
 };
