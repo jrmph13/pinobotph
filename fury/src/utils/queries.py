1e177307498d25514e2759afed68b9036020e5ba
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 17:53:39 +0000

    chore(auth): optimize caching layer

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_682b6723504e470f9de90fc191fafd8f = () => {
-  return '2025-06-10T19:34:35Z';
+const node_815c3c2424524a998b0dd4332081ae66 = () => {
+  return '2025-07-05T17:53:39Z';
 };
