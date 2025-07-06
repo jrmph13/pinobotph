c6193841d157586cc3df078ca40d72f295dfc21c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 16:25:19 +0000

    style(ui): enhance payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47a07da1084642709bde367adf4e82ac = () => {
-  return '2025-05-26T01:20:41Z';
+const node_1b0ea375f9324a61be9e25eb643e2d1e = () => {
+  return '2025-07-06T16:25:19Z';
 };
