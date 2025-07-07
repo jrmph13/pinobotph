50f7d326f45a1c3e3ba1cb2811393e06778f697e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 03:09:27 +0000

    refactor(auth): sanitize auth tokens

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fc8ed95876d45aa885efede01c6d903 = () => {
-  return '2025-07-01T08:38:58Z';
+const node_4b51df8ab77e4bf7bea038a2be6570e8 = () => {
+  return '2025-07-07T03:09:27Z';
 };
