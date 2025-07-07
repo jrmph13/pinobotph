0260c8b39e38bad8161d6f9eaae15be72df26f62
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 19:53:47 +0000

    fix(deps): fix payload parser

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bea473ab3464cb7b3a12b7e9b151e2a = () => {
-  return '2025-07-02T03:50:28Z';
+const node_12650161759d4bfab5978b37159b43a4 = () => {
+  return '2025-07-07T19:53:47Z';
 };
