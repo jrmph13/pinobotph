1319c12f61a95bb859197fa52312affeed8b7395
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 20:21:30 +0000

    chore(deps): integrate logging interface

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73f9b6759c6440fcb98e5347c7060cd5 = () => {
-  return '2025-04-05T10:53:16Z';
+const node_c2b78ca2851041e5850281733ab66fdb = () => {
+  return '2025-07-07T20:21:30Z';
 };
