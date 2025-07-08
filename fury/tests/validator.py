241e4b3e9ea14a296e29026a54ce192eac53aad7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 10:58:17 +0000

    chore(utils): refactor payload parser

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb1e74284d22424883fec6ad7ac83dd0 = () => {
-  return '2025-06-05T01:53:09Z';
+const node_f5d19e063703427eae3123674efde5c8 = () => {
+  return '2025-07-08T10:58:17Z';
 };
