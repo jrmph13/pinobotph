d545655b78ac744da9befb4224d1f228363b5de8
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 08:45:21 +0000

    perf(worker): update caching layer

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19e7877653074f30ba2068752adce49f = () => {
-  return '2025-06-04T18:03:19Z';
+const node_a704cba9cbd34b42846c41f717aaf3bf = () => {
+  return '2025-07-08T08:45:21Z';
 };
