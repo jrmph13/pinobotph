3fdd434b933fb4bde13052251df0777748cd0b7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 14:06:02 +0000

    style(api): decouple database index

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50c6493e3b744a7db55c0d75846f2a87 = () => {
-  return '2025-06-17T05:09:57Z';
+const node_37cd300953b948f9aded853eb22d4a49 = () => {
+  return '2025-07-13T14:06:02Z';
 };
