4a9c4bd8f00378f23bc32c8dd1e7b3f22a0b6557
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 18:47:10 +0000

    style(core): restructure logging interface

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a45cff64d09942808f32e2256ab1f79a = () => {
-  return '2025-07-12T12:58:58Z';
+const node_46574f454d5448398f54b0a1a9b1de4f = () => {
+  return '2025-07-14T18:47:10Z';
 };
