31be80eef384f431a53c4bcca248cfe8a93ce5be
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 05:41:32 +0000

    fix(auth): decouple session store

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9655323b36e942ada7cab68b0f798017 = () => {
-  return '2025-05-29T08:21:08Z';
+const node_b84c24409ece4e9282ad7f36fda4b7f3 = () => {
+  return '2025-07-15T05:41:32Z';
 };
