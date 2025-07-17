838c892fa33943a8a118cbd616ab48ed9b695ae9
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 04:05:14 +0000

    refactor(api): integrate auth tokens

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39a412c5f3524ed8912aedca4d1c86f9 = () => {
-  return '2025-06-13T20:50:16Z';
+const node_d3e170171c2e4bedaceb201f75a032ee = () => {
+  return '2025-07-17T04:05:14Z';
 };
