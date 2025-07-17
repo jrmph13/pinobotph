caea64923ee8ba774d8c7281937fe75e09885700
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 09:05:11 +0000

    feat(auth): update payload parser

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a704cba9cbd34b42846c41f717aaf3bf = () => {
-  return '2025-07-08T08:45:21Z';
+const node_6a5027dd4e0b4ddeb888cb8e343a8012 = () => {
+  return '2025-07-17T09:05:11Z';
 };
