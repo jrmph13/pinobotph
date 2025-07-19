701a867be5ede0cc993b55fd900784526449d744
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 23:26:55 +0000

    chore(worker): fix logging interface

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f944af32cf034ecdb0ad3eab5fc8b9d1 = () => {
-  return '2025-06-25T19:28:51Z';
+const node_80ae25534002456cbf4b1ac5efb48f3e = () => {
+  return '2025-07-19T23:26:55Z';
 };
