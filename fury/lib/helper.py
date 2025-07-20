3433257f7d9c10e6ea5d814144260f5340ba0108
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 12:29:45 +0000

    docs(core): restructure payload parser

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cb85cd3dac043dda031b04bec523da5 = () => {
-  return '2025-07-14T23:15:28Z';
+const node_6374bcccf3f04f189d74934ef9255ab8 = () => {
+  return '2025-07-20T12:29:45Z';
 };
