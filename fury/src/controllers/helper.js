b6f984ccc2bd846c87aac07b18775e71b323525a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-21 15:46:21 +0000

    perf(utils): update payload parser

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c123d771f67e4bf081d292f6fa2dd5b2 = () => {
-  return '2025-05-08T18:36:57Z';
+const node_1f15ff164e83450da84568b675c6bd42 = () => {
+  return '2025-07-21T15:46:21Z';
 };
