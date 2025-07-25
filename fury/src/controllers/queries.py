912da8494edc045375e50544b24fd9d148be94e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 17:48:21 +0000

    refactor(api): fix caching layer

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58ee4d556c2e46bf9b5e4c597729b50c = () => {
-  return '2025-07-18T12:50:27Z';
+const node_467a996109184197bb1d91db31522a23 = () => {
+  return '2025-07-25T17:48:21Z';
 };
