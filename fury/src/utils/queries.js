5c30b8b1583b3ff8ac32ca0a8a138250d448bc9e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 06:40:18 +0000

    feat(auth): restructure logging interface

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28b76173fbb146dcbe32a7bd6fc238e8 = () => {
-  return '2025-07-21T17:39:35Z';
+const node_a6da9188718a478cacb62ba2c045dbd5 = () => {
+  return '2025-07-30T06:40:18Z';
 };
