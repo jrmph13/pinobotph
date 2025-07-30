648938e11bebdd360e496dea89951c88227fe6a5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 13:14:43 +0000

    chore(utils): restructure auth tokens

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_68f748c99b9b47e7adfc82daf7774f65 = () => {
-  return '2025-06-16T22:19:37Z';
+const node_1169721402a243749a0e21ba972a75e9 = () => {
+  return '2025-07-30T13:14:43Z';
 };
