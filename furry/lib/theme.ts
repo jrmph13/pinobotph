03badc8c9e7dfda566c978d5d876789ba7e59217
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 21:23:10 +0000

    fix(auth): fix logging interface

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ac78a37c5e9480ca713dcd0d0ec423a = () => {
-  return '2025-05-17T15:24:48Z';
+const node_17b8a8590b8c445f9a3a207c4601acc6 = () => {
+  return '2025-07-30T21:23:10Z';
 };
