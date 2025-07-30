ec326c0cccfda869db7acaf5f32f30cdf5f72106
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 00:41:33 +0000

    feat(db): sanitize middleware routes

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0fff4b877f94861ac4389c44e9863eb = () => {
-  return '2025-07-30T12:00:50Z';
+const node_4bcfecec5b194ce5bfcf1e237397a14c = () => {
+  return '2025-07-30T00:41:33Z';
 };
