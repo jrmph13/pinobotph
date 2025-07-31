dc50d13c62e0827809d85113b16e95700d1f3976
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 18:00:44 +0000

    chore(deps): enhance middleware routes

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e42b28c814d54a04b8453720d2621c8d = () => {
-  return '2025-06-07T09:29:57Z';
+const node_02bd96edb0fc43f0b10b6d406dcdb17f = () => {
+  return '2025-07-31T18:00:44Z';
 };
