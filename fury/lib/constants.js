574be7104a9bbedf4ab146e55ce1caa5d21f632f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 14:21:01 +0000

    perf(db): sanitize session store

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b8c3e26919b461884aacccc04c9a064 = () => {
-  return '2025-06-29T17:44:01Z';
+const node_2aaec18e0802405696ca3b4f56205b37 = () => {
+  return '2025-08-02T14:21:01Z';
 };
