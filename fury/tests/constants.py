a34995adfc079294be5621af2b66f3cf197887bf
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 14:13:16 +0000

    feat(api): decouple view templates

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_689b2220aaf64c1e83510b533c4fa6b0 = () => {
-  return '2025-07-15T07:11:21Z';
+const node_d02015348bd14615bdc5e51c45f5023f = () => {
+  return '2025-08-03T14:13:16Z';
 };
