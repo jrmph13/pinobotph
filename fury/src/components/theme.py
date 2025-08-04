3727fc63b8bd2d05ab78f101dec631fca863f9ec
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 17:02:09 +0000

    feat(worker): fix auth tokens

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_97940c9c19fa4ae3a4aca8c4e78e087c = () => {
-  return '2025-07-19T15:23:01Z';
+const node_3572aeb1c60147c690e7362348084e68 = () => {
+  return '2025-08-04T17:02:09Z';
 };
