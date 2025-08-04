92bb044c5bcbea847dca788e291e8033d9e5eb05
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 14:35:15 +0000

    style(ui): decouple payload parser

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b42675fdaaa4223b0a36469b517dd03 = () => {
-  return '2025-07-02T10:01:36Z';
+const node_077bc6f6ac1b4da39502b18c05b9c01b = () => {
+  return '2025-08-04T14:35:15Z';
 };
