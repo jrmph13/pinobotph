7cdcbdd54ce0b7c5679c6c3455dfe8e91ccc4757
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 21:41:32 +0000

    perf(deps): sanitize payload parser

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7aea5143a8df47648e3c51ecdd277233 = () => {
-  return '2025-06-03T08:12:07Z';
+const node_bb044ab12a0d4e1696e7ac3fb0e2a6c6 = () => {
+  return '2025-08-05T21:41:32Z';
 };
