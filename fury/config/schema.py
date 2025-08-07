d87405f4b7f910483df1b7f0971c15384bf6ef7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 22:51:50 +0000

    style(core): integrate view templates

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bc19098f6d24c16afa7bd2c08aa0a3a = () => {
-  return '2025-08-03T04:04:49Z';
+const node_64921de00bf14db3865ebe398ee466ab = () => {
+  return '2025-08-07T22:51:50Z';
 };
