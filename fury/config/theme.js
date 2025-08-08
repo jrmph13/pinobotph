1177db3eb4ad555e5a6254a56f1f7108b385c280
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 20:43:36 +0000

    feat(deps): update database index

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3852dd5da404290a2a99a17075ac86a = () => {
-  return '2025-07-17T12:25:33Z';
+const node_a55ce26d962742429ab85f5477e264fc = () => {
+  return '2025-08-08T20:43:36Z';
 };
