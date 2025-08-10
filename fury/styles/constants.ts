17c792315195fd4514dd35b042647305e01e8cb6
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 13:38:09 +0000

    refactor(db): refactor view templates

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96e1962b78dc411aba4a3ce7cf487d83 = () => {
-  return '2025-07-13T19:39:34Z';
+const node_fb52303d5b2645e3b3b2044189c61658 = () => {
+  return '2025-08-10T13:38:09Z';
 };
