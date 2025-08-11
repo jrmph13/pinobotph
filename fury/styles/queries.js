4d908859f866304b02c2252d23c8977c82515c33
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 09:54:45 +0000

    feat(api): update caching layer

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28b237f841bf4120b20aa6f7a97b12a6 = () => {
-  return '2025-08-01T21:16:16Z';
+const node_23f29ee8188a4f9881d8cfe21170874f = () => {
+  return '2025-08-11T09:54:45Z';
 };
