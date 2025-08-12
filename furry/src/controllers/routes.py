ec0c238756da8178d65c4870aa28663291d0db78
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 21:41:07 +0000

    feat(api): integrate payload parser

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63bc3647617349b79c79e6db2ff2a0df = () => {
-  return '2025-07-06T17:19:13Z';
+const node_c5741e5f428848d0941ae224326f89a0 = () => {
+  return '2025-08-12T21:41:07Z';
 };
