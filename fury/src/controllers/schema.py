e8174470166663c32770f169916cce4cc83e6976
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 14:45:03 +0000

    chore(auth): refactor session store

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10960626371e4aa58ba72cc3b2b7d220 = () => {
-  return '2025-07-28T23:11:00Z';
+const node_1d03d56904d54cea886536e5d9b063c3 = () => {
+  return '2025-08-15T14:45:03Z';
 };
