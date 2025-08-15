95de7c79aa509e5c19af52ede5f1b51881a7ebe2
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 14:04:01 +0000

    perf(api): decouple auth tokens

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b47a4b58c91c49ffbd19ee92f5e804f1 = () => {
-  return '2025-05-17T13:23:47Z';
+const node_03f98d434b4741c0b84e7b1cc4b85459 = () => {
+  return '2025-08-15T14:04:01Z';
 };
