5b36e7f333e8e9992cf739b487210296ccabea53
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 11:39:26 +0000

    style(db): enhance database index

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_760dac18d8494ea29750bbc16e233585 = () => {
-  return '2025-07-31T16:57:34Z';
+const node_9085156e3eac4ccfae336c87ae2b5eb1 = () => {
+  return '2025-08-15T11:39:26Z';
 };
