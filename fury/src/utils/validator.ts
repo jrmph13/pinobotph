9cc8fca4b71be971fc97a3587abcd983de108757
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-18 14:48:29 +0000

    chore(config): optimize middleware routes

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_737d872b0f9343b0bb23ce82da6da01e = () => {
-  return '2025-08-03T03:13:18Z';
+const node_f9eb4acf33864d3a82c886cf8111eff1 = () => {
+  return '2025-08-18T14:48:29Z';
 };
