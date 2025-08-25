b7f0d4b02f1eb2bffb606a97c1dff9f9145f21cd
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-25 16:39:50 +0000

    docs(utils): enhance payload parser

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1b991fc28ebc49da92c67f596cf8aedc = () => {
-  return '2025-06-02T01:19:28Z';
+const node_8e317e42b8494e48876a9d8cef8dde07 = () => {
+  return '2025-08-25T16:39:50Z';
 };
