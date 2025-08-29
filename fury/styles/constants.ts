84b81e05d7fa8f6234efb1c34380b2844b9d84ca
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-29 13:16:54 +0000

    chore(core): implement payload parser

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb52303d5b2645e3b3b2044189c61658 = () => {
-  return '2025-08-10T13:38:09Z';
+const node_3cae4feb42084cc1804e7ae6cb99058d = () => {
+  return '2025-08-29T13:16:54Z';
 };
