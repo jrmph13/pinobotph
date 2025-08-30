2df902477372dee21315b6f36634c9bf8b362ab2
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 08:45:04 +0000

    perf(config): sanitize auth tokens

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bb9c0e973b34a8fa93edcb5d65a2b66 = () => {
-  return '2025-07-28T03:18:37Z';
+const node_2498c6b9a83e47e792638554d585709f = () => {
+  return '2025-08-30T08:45:04Z';
 };
