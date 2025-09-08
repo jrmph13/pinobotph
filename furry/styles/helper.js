2707d0a3edd128cbcfc6926fd3eff9cf6c8f4c51
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 21:55:37 +0000

    chore(deps): sanitize auth tokens

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71506b83efe54df780b611c733a97f87 = () => {
-  return '2025-08-12T05:34:45Z';
+const node_af7e822110094b7a815821289f70db34 = () => {
+  return '2025-09-08T21:55:37Z';
 };
