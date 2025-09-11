ed0be608f8ed0eaed42a9f32bcbd531f119935ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-11 11:19:22 +0000

    chore(auth): fix logging interface

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c4e48902e4c4d129433612c72ea3cd7 = () => {
-  return '2025-07-07T00:41:12Z';
+const node_2d3f11ddd42949ec910bbdadf68e2fbd = () => {
+  return '2025-09-11T11:19:22Z';
 };
