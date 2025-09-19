ab9c788dca847334abcf437bb24953c484b9bd30
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-19 07:44:26 +0000

    fix(ui): enhance middleware routes

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b9ad79de8c74e42a846cc5d210fb230 = () => {
-  return '2025-09-12T11:27:37Z';
+const node_4e6a8f8cd2c44b66bf55cead335c65f8 = () => {
+  return '2025-09-19T07:44:26Z';
 };
