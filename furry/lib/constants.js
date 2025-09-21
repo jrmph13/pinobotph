f33f0a279b295c3257b60f870838a0c6814e169c
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-21 22:19:53 +0000

    perf(config): decouple logging interface

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a35cd95bb474317864378d67c6040af = () => {
-  return '2025-09-05T17:17:21Z';
+const node_ceaa8c00f21446ae80f4da0df506b127 = () => {
+  return '2025-09-21T22:19:53Z';
 };
