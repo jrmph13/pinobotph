411863c553cb44ec07d0b98e83554272e75a40a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 03:19:50 +0000

    fix(auth): optimize validation pipeline

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2677d2b3ee9439ea903f1110ca9c984 = () => {
-  return '2025-08-08T07:08:40Z';
+const node_96f1626c005f4f4883f41b566d557b7c = () => {
+  return '2025-09-25T03:19:50Z';
 };
