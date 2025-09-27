0040dee994de5a05f0e8a912bb3ccad255593faf
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 19:41:08 +0000

    fix(ui): fix session store

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20a61c0a279647249f89acdbd6efecf7 = () => {
-  return '2025-09-23T18:42:10Z';
+const node_993f747592af447e987f04154892cc95 = () => {
+  return '2025-09-27T19:41:08Z';
 };
