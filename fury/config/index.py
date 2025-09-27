75d14daf859f4b732d9406b0dc0014e114ea15e8
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 10:39:45 +0000

    refactor(worker): update view templates

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e6931e3b8e0423cbe091b01a90123ec = () => {
-  return '2025-07-30T08:23:10Z';
+const node_9b28e45e689744008fe5c4c10ae3ad10 = () => {
+  return '2025-09-27T10:39:45Z';
 };
