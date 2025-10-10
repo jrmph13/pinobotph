89128b5c85117a672b1413cff62b1ad8abcef8f0
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-10 18:59:59 +0000

    docs(auth): fix view templates

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4201fe51e0bb48bd911461dcf030d561 = () => {
-  return '2025-07-02T17:57:09Z';
+const node_a8b10766491a43e3857763d19ad74814 = () => {
+  return '2025-10-10T18:59:59Z';
 };
