a2a948b53c1a92c891d3ed129b11e2f024ade07f
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-16 03:23:34 +0000

    feat(worker): implement payload parser

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a1c78f2cd43485ea4d37850c847c794 = () => {
-  return '2025-08-15T17:34:42Z';
+const node_18fc85affa39457f9d5d4ed4f7d2db75 = () => {
+  return '2025-10-16T03:23:34Z';
 };
