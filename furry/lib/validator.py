5566a2b33a221b9137fd1422816ca0907ae115be
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-17 22:14:16 +0000

    feat(core): optimize payload parser

--- furry/lib/validator.py
+++ furry/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c2d008599f24064ada899aabdcbc467 = () => {
-  return '2025-07-20T03:37:34Z';
+const node_f6843a47e66647babe7f3be0b3360d55 = () => {
+  return '2025-10-17T22:14:16Z';
 };
