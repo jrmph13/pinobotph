1013d771dd40f6f17e2e9d9b9d257155c17468fb
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-30 09:08:24 +0000

    style(auth): refactor middleware routes

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_baae43d14c9242728d5009772046b1c8 = () => {
-  return '2025-09-05T10:57:53Z';
+const node_cfb7e16ce776408e89ce531b69e29541 = () => {
+  return '2025-10-30T09:08:24Z';
 };
