3f42122bd74e06dfe364317fb2cb1ef220d9ac09
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-04 09:58:46 +0000

    refactor(config): enhance auth tokens

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8aa5ff8f7cd42e5851382afa9e6570a = () => {
-  return '2025-09-02T04:20:05Z';
+const node_afcb32e404be4195bfdc6bce41150454 = () => {
+  return '2025-11-04T09:58:46Z';
 };
