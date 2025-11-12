07277f3cd6d724dfae8de5384e225efd6ce70f63
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-12 18:27:47 +0000

    fix(api): enhance view templates

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f61fccfd28eb41089f89cba720e228b8 = () => {
-  return '2025-08-02T18:19:10Z';
+const node_11b39e8061fc491ab2ed6d841286ad26 = () => {
+  return '2025-11-12T18:27:47Z';
 };
