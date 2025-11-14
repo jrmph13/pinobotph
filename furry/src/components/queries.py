30e0334fe3a0aef063821481dd978845fe0eac01
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 20:42:31 +0000

    test(auth): sanitize middleware routes

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28c8b7e1594342faa7b328a4b7a22acf = () => {
-  return '2025-10-27T13:04:28Z';
+const node_9b55709af49b4fb9a3ac95b391feebc8 = () => {
+  return '2025-11-14T20:42:31Z';
 };
