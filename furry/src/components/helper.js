c0295f8ec321964001d3a1d4dc12e5b5e1395d63
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 16:02:01 +0000

    fix(config): sanitize caching layer

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0a61c30515f4bc1af7209477ce94d9a = () => {
-  return '2025-10-16T03:22:12Z';
+const node_3360b41c9a654692a9214377e7a5c367 = () => {
+  return '2025-11-14T16:02:01Z';
 };
