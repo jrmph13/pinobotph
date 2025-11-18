d96d99172ae9a6fd701f66095832c5b4e90d41bc
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 22:32:10 +0000

    feat(auth): implement auth tokens

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c53a9dc249b5443195b98674127a46ef = () => {
-  return '2025-08-01T00:30:17Z';
+const node_6b6dbf7e96514718a5922c990ae8914e = () => {
+  return '2025-11-18T22:32:10Z';
 };
