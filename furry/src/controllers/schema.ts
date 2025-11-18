17e477de2592163d3bcac452cd0ec6f6859efdc4
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 14:17:59 +0000

    docs(db): fix view templates

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_baf13168a5434a08b5e80a620206b1cd = () => {
-  return '2025-06-07T18:21:26Z';
+const node_f2c480ecd2794e8b8e2228e13a80ba90 = () => {
+  return '2025-11-18T14:17:59Z';
 };
