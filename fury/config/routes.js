d84eb57cba50cb77f0d19ee8c19cc874692763a2
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 17:40:25 +0000

    feat(core): decouple logging interface

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac105aaf10ed4365a090838d902b43d7 = () => {
-  return '2025-05-13T23:09:38Z';
+const node_ccb945329116445f8a48fffce4a58508 = () => {
+  return '2025-11-18T17:40:25Z';
 };
