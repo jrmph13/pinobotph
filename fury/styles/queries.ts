54d51c988eb0319d18a6d12b80d0a4290046c900
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 19:37:27 +0000

    fix(deps): enhance view templates

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3adda1a791c4299b6bca757444d48ce = () => {
-  return '2025-08-08T13:48:27Z';
+const node_2f28c17eff7946ee95b78f1d122ffede = () => {
+  return '2025-11-24T19:37:27Z';
 };
