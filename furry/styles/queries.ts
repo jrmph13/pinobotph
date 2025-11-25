432eee51d97de5426218fb465d157392961370ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 08:51:36 +0000

    docs(config): update auth tokens

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cab46440734d470a9f9bea3ded23c503 = () => {
-  return '2025-05-08T21:50:49Z';
+const node_1d6535727b7c4a0ea1ede3edf13f2c03 = () => {
+  return '2025-11-25T08:51:36Z';
 };
