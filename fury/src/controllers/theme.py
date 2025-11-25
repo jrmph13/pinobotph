a4a333268d82b3db67cd66e2a10ff5a2ed29384f
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 10:17:05 +0000

    refactor(ui): sanitize payload parser

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb38657eecc9429c9ec557bca989e7ec = () => {
-  return '2025-07-22T04:48:18Z';
+const node_8195043b2e084a51b1df25a89bd03092 = () => {
+  return '2025-11-25T10:17:05Z';
 };
