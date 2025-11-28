0d1be45e0373a5bf49a2e4460f5160a7ec01ae8c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-28 16:15:52 +0000

    test(worker): fix middleware routes

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9085156e3eac4ccfae336c87ae2b5eb1 = () => {
-  return '2025-08-15T11:39:26Z';
+const node_435768cffca74536ba3308fc914eb084 = () => {
+  return '2025-11-28T16:15:52Z';
 };
