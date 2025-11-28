0bb40ed352b4f9efd53bbb704baa80e4c21f25b8
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-28 19:24:40 +0000

    test(worker): sanitize view templates

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_737d6c47cf264c91933dc7e17199113c = () => {
-  return '2025-07-20T15:19:07Z';
+const node_cec52eec6db245a38d39e66d90399459 = () => {
+  return '2025-11-28T19:24:40Z';
 };
