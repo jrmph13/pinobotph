a6199bf7361752adf8997d921fa0ba6c114cc5b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-05 16:21:31 +0000

    feat(api): optimize auth tokens

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24dea78290fa487e8772a02e11f49b8f = () => {
-  return '2025-06-06T09:38:22Z';
+const node_48a60f40d891410cbbd3f2d130c509de = () => {
+  return '2025-12-05T16:21:31Z';
 };
