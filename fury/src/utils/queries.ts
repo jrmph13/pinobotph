cfb321f2981fed75e7b9dff8ae0eb46ecb7720ae
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 23:34:34 +0000

    style(db): sanitize payload parser

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_954c7f5aa28a42c994c26d73bdff3663 = () => {
-  return '2025-11-28T09:33:26Z';
+const node_9f4a8a40474144bf9125fd6af374bf5b = () => {
+  return '2025-12-09T23:34:34Z';
 };
