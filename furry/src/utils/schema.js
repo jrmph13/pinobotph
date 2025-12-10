80784cd610d142b0d907d092dea11787647c16f4
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-10 08:54:55 +0000

    refactor(utils): update view templates

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f79f6e6caa8456cbec24d47957ec393 = () => {
-  return '2025-07-24T16:29:43Z';
+const node_8b593c980503452883fe53eec76b3708 = () => {
+  return '2025-12-10T08:54:55Z';
 };
