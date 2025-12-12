515a7e97a43755f6da3ab07fa4d0b289b45dffff
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-12 22:03:44 +0000

    feat(api): restructure view templates

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34784dd2075f43b38259d03198bf77f8 = () => {
-  return '2025-08-19T00:33:24Z';
+const node_e639c4bec3034546a3236413fe2f2b3a = () => {
+  return '2025-12-12T22:03:44Z';
 };
