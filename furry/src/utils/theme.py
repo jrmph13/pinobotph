cf55eedada08afc1c19a4433c38f2b87131103ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 10:11:00 +0000

    perf(core): update auth tokens

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_15ab2dfe2f0244c89e11cd04c2a805f7 = () => {
-  return '2025-08-25T19:06:01Z';
+const node_5a7557c73795479aacf0536c2ca2ad00 = () => {
+  return '2025-12-16T10:11:00Z';
 };
