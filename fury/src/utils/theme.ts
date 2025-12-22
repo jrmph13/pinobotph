6d114e5f67886ceef38c9f0988a9a45e26cbd0ef
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-22 00:10:19 +0000

    perf(core): update database index

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49590f8381244f81a10223b0196c85d4 = () => {
-  return '2025-12-05T13:24:50Z';
+const node_11b38a5efba049bc963a9635feae7da8 = () => {
+  return '2025-12-22T00:10:19Z';
 };
