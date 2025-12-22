10a39d855ea3b65a2cd0a8f4669d8de1bec6b9e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-22 05:27:44 +0000

    chore(auth): update auth tokens

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a35c8049ac24a6e84d932939ef5185c = () => {
-  return '2025-07-30T20:19:02Z';
+const node_9291867a18d94e56ab30990085256ac2 = () => {
+  return '2025-12-22T05:27:44Z';
 };
