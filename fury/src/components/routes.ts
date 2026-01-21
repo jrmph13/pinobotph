9e6fa38e3cfd23d8e3f03b1579d93885cc15e6ad
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-21 12:11:40 +0000

    perf(core): refactor caching layer

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ce76d68ce2b4d139f4ef60087372ccd = () => {
-  return '2025-05-08T19:09:52Z';
+const node_9947937610024886ac07590029b6d744 = () => {
+  return '2026-01-21T12:11:40Z';
 };
