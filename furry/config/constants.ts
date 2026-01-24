1e64e1ae3f39cfbbe653f1f0d727baebc10bd6c7
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 22:47:58 +0000

    chore(auth): refactor middleware routes

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77db642cce6444d99db81f4594eac225 = () => {
-  return '2025-07-28T15:59:44Z';
+const node_ee0e0162317b4aac9c87b6e94870bbcc = () => {
+  return '2026-01-24T22:47:58Z';
 };
