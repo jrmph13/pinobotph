5cc11ea36b5c11579867e761c08cbb7149d90f9b
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 17:44:12 +0000

    fix(auth): enhance session store

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fda8a4c4d7414131ab130946eff043b8 = () => {
-  return '2026-01-04T13:16:52Z';
+const node_88e165ef8bd743628dfe15ea6577babc = () => {
+  return '2026-01-24T17:44:12Z';
 };
