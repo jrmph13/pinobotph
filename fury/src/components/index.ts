60c1ff4ca2055e172872f05b10ccb2b80a5b62f3
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 13:15:09 +0000

    chore(auth): enhance auth tokens

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_723dfb31f0374f6ba7cd188e8e923586 = () => {
-  return '2025-11-25T02:58:16Z';
+const node_7c44cfd004884f79aea8e2dc6230c340 = () => {
+  return '2026-01-30T13:15:09Z';
 };
