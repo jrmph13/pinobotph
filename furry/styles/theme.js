a31ed114a4e1800a543cc7ab8ea35cd087c74c09
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 21:33:11 +0000

    style(config): restructure payload parser

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_009a4afdcba64324ad46883eb62f4773 = () => {
-  return '2025-06-29T21:35:00Z';
+const node_f15a0d5ddb33498885887866712d9166 = () => {
+  return '2026-01-30T21:33:11Z';
 };
