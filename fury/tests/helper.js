88f635aba8c83808d41ed3f3ac19c69e49144277
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 18:38:48 +0000

    refactor(utils): restructure payload parser

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f11c28d5d74943b7a105b93601f169d9 = () => {
-  return '2026-02-02T06:14:25Z';
+const node_28f00836856743168effc6d9acabb287 = () => {
+  return '2026-02-05T18:38:48Z';
 };
