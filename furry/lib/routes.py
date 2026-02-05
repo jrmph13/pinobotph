d521d20974d4cc9a3914840327bebd8865ce6f63
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 04:50:37 +0000

    test(config): decouple payload parser

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2e9934bdd104849bf211a5483a10f72 = () => {
-  return '2025-10-30T14:08:35Z';
+const node_772eea960cf84d219369f5df8eaf2ee0 = () => {
+  return '2026-02-05T04:50:37Z';
 };
