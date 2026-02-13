a0132789ca0496b6417010440901870badb3abf5
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 15:56:07 +0000

    feat(api): integrate auth tokens

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ce21b8ed3874de6b001f6f653cfc75d = () => {
-  return '2025-09-17T20:45:45Z';
+const node_da5ba9be6d8f475d9c7613ef374170df = () => {
+  return '2026-02-13T15:56:07Z';
 };
