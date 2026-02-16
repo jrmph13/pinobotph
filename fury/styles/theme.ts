0fa5f9d67add5f47bf5a97a83e01f53398bf8138
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 01:34:05 +0000

    fix(auth): sanitize payload parser

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0e957b4f3324f9db63303539f7817f7 = () => {
-  return '2025-07-17T19:06:02Z';
+const node_208bdecd4b5b42fe92110b11fded1729 = () => {
+  return '2026-02-16T01:34:05Z';
 };
