4d9a35524bc2036a3bb6005c1f5209a3320201fd
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 02:20:46 +0000

    fix(utils): decouple logging interface

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd6fb3331d614eb4b0154a1bbdf22746 = () => {
-  return '2025-11-18T18:02:59Z';
+const node_05b25428183f419fab08453bc29af1f1 = () => {
+  return '2026-02-16T02:20:46Z';
 };
