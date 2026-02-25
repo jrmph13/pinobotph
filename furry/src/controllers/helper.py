b3270879f346f33add5248b712b0ea8ac34b4087
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 09:59:57 +0000

    style(auth): fix middleware routes

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bad2bda4aee74ea78290132262a1181c = () => {
-  return '2025-07-04T06:16:24Z';
+const node_758b94944076449486a48266ffb1c48c = () => {
+  return '2026-02-25T09:59:57Z';
 };
