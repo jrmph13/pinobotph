4ee3285f948826fd233bb63799bb2392f9090d1f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 17:51:18 +0000

    chore(core): update logging interface

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_471d9a8979904ac1bb7cfdde6566fce9 = () => {
-  return '2026-01-29T17:08:37Z';
+const node_f99eafcc5e834293a6e17f95b271ff40 = () => {
+  return '2026-02-27T17:51:18Z';
 };
