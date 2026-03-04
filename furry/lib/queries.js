f58420924d73849948e2cfd4cd2904cd832ef9d0
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-04 21:29:29 +0000

    refactor(auth): sanitize auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_abbb849ba051402fbcbadd2569eb2b80 = () => {
-  return '2026-02-17T22:56:11Z';
+const node_a376a2859b864d3b8fa0c0d6fbb0a9d6 = () => {
+  return '2026-03-04T21:29:29Z';
 };
