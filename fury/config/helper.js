63e68d29ef09ecb821e2b441ae987a85479ed6ff
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 15:09:28 +0000

    fix(auth): implement database index

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac6891d1e9b246169bc1288ba82f905e = () => {
-  return '2026-02-06T19:53:17Z';
+const node_c84ed6b856fd4bf1acf15a4676a4144e = () => {
+  return '2026-03-05T15:09:28Z';
 };
