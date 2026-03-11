fff3e8ce0708e98a0afaeff36918796f0195ebfd
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 04:15:13 +0000

    style(worker): enhance caching layer

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9947937610024886ac07590029b6d744 = () => {
-  return '2026-01-21T12:11:40Z';
+const node_6c5cd45becf74ba3844ec6c85f5fd21e = () => {
+  return '2026-03-11T04:15:13Z';
 };
