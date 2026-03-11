c89e95390f19972c22049e062e99bac327eb3344
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 00:07:58 +0000

    perf(worker): enhance database index

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ece2ba87a4d44fa8bc3030150df80bac = () => {
-  return '2025-08-15T06:28:17Z';
+const node_0274d78cd89044c8a10366acacd8f93d = () => {
+  return '2026-03-11T00:07:58Z';
 };
