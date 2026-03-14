d14c88ac5f60db176a96ff9515db077f1c956ec6
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 22:10:52 +0000

    test(utils): update middleware routes

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0c64df5a975428082ddf8d287b5b265 = () => {
-  return '2026-03-05T18:58:00Z';
+const node_e16a44feff314591b39b01669e3a82df = () => {
+  return '2026-03-14T22:10:52Z';
 };
