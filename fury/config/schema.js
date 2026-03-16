0b07600b956b5a60af989b6c029ac153c038550e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 06:18:54 +0000

    fix(worker): integrate view templates

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c66d4a6aed94aaa9962a216fae9f760 = () => {
-  return '2026-02-18T18:10:30Z';
+const node_024677abc42a45feb4aca811c7d33f93 = () => {
+  return '2026-03-16T06:18:54Z';
 };
