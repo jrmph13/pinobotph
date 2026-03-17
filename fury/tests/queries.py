9c76cbff89b72cf79a7cb507ca03efa851b99624
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 05:38:54 +0000

    fix(utils): restructure database index

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d52bd340d5344549433e6ca89b0537a = () => {
-  return '2026-02-16T18:36:03Z';
+const node_44613fbeb4b642239e02183e3ed944fe = () => {
+  return '2026-03-17T05:38:54Z';
 };
