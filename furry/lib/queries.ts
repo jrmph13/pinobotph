56a72e3f523944735c242133a1c69dde303a5618
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 03:32:12 +0000

    test(worker): sanitize middleware routes

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3097891ada54bba9d09debb61defd8e = () => {
-  return '2026-02-16T16:47:51Z';
+const node_01ad74fce33c413089af8d7e3f7404c7 = () => {
+  return '2026-03-20T03:32:12Z';
 };
