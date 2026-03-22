dc66ee33b15ed4c41be9d4e3bc999555e7f9a914
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 18:42:47 +0000

    refactor(ui): update auth tokens

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fcf630a8ccd4424a99b00f2c7f842a64 = () => {
-  return '2026-03-04T13:31:12Z';
+const node_96be46f7794f477592817c633cd273c9 = () => {
+  return '2026-03-22T18:42:47Z';
 };
