622f3d5e38854cfb443eff5ce9c39f02e95f08e4
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-24 17:06:57 +0000

    refactor(config): restructure middleware routes

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9dce61d161542a9a8ea454c257d01a6 = () => {
-  return '2025-08-08T05:45:52Z';
+const node_e7259822eb1847b196b9bc87d468ebad = () => {
+  return '2026-03-24T17:06:57Z';
 };
