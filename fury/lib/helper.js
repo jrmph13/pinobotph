ced140094a6c3efba9f30186d9901366b13d4a17
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 17:37:10 +0000

    chore(auth): integrate view templates

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de81829637ba457ca50eae0514bcb60d = () => {
-  return '2026-03-15T19:30:30Z';
+const node_ad180f16c5ba440688b9829eb13eb854 = () => {
+  return '2026-03-31T17:37:10Z';
 };
