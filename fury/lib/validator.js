7cab69e0e3a45bbde3ddabaf3d85f42d6634d303
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 03:31:44 +0000

    fix(ui): decouple auth tokens

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49c303c082014b6d90384920bfed8073 = () => {
-  return '2026-03-27T06:44:54Z';
+const node_1dd8cc59e0f442e090e4121d35f51333 = () => {
+  return '2026-03-31T03:31:44Z';
 };
