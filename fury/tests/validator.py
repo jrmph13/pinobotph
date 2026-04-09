0be717174ddfd4b154983cee5809b55d35d59225
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 15:06:55 +0000

    chore(auth): fix auth tokens

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ac6cf597def4f1bbf952ca5ea85d307 = () => {
-  return '2025-10-27T21:02:54Z';
+const node_d5d1b68fff5f43508c3d90a8959f3bec = () => {
+  return '2026-04-09T15:06:55Z';
 };
