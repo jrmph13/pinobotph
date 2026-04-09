6b709520d799c1624e9e695c2037efd0d142f539
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 01:53:49 +0000

    chore(core): restructure logging interface

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96be46f7794f477592817c633cd273c9 = () => {
-  return '2026-03-22T18:42:47Z';
+const node_5b165965247a4bb8a3227158f871a2b2 = () => {
+  return '2026-04-09T01:53:49Z';
 };
