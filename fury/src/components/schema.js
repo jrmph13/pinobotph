0c3ec190d45ca3288238ea48fb7c2c57f18f7ab0
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-10 21:56:01 +0000

    feat(config): decouple auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec68eb6ed7c24e1d8efe2bb10e2d5454 = () => {
-  return '2026-02-14T20:19:41Z';
+const node_d684ab2dfc0c4d6b97c27048ef67f2cd = () => {
+  return '2026-04-10T21:56:01Z';
 };
