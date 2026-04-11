d7d959b431df9b8e64305e9b82fb85e9a9742169
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 11:43:33 +0000

    fix(worker): integrate payload parser

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f45a434f75df42c9b546e20bb2e341e0 = () => {
-  return '2026-03-06T13:08:30Z';
+const node_75e0daa7bc1d4fa3bc5a5a317d679062 = () => {
+  return '2026-04-11T11:43:33Z';
 };
