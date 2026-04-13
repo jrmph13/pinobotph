095d53054ae21250b76c838ac7375764aa473c83
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 06:33:49 +0000

    refactor(ui): update payload parser

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb499296b6134750879f54b8ae937508 = () => {
-  return '2026-02-12T05:14:02Z';
+const node_6c4e3768685e47e7acb22b2d4379ef7d = () => {
+  return '2026-04-13T06:33:49Z';
 };
