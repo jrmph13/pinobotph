3639209e3516e9f1560cde32debb809d73c356df
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 03:23:52 +0000

    fix(auth): sanitize logging interface

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0598618f2fa145f1babf77544f687a9e = () => {
-  return '2026-03-25T10:36:42Z';
+const node_16370f8fc354446caf1d0dadd9b4dbd9 = () => {
+  return '2026-04-14T03:23:52Z';
 };
