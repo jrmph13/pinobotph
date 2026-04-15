893653440d6f8a98308aca887cf96b2a36ede5cb
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 20:31:53 +0000

    perf(auth): update session store

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38c7f74bb2744a9bb4084f556304d85d = () => {
-  return '2026-04-03T06:50:21Z';
+const node_109a43225dc24e659ffc47a78e580453 = () => {
+  return '2026-04-15T20:31:53Z';
 };
