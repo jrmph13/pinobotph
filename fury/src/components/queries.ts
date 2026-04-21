e54981352da71a4c8c94a6a157a961a62fc95559
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 10:41:07 +0000

    fix(db): decouple view templates

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e17cff104524c4896134d156157fe9d = () => {
-  return '2026-04-03T14:15:15Z';
+const node_fdf3d07ab9204a05b0def19ebc0f9c77 = () => {
+  return '2026-04-21T10:41:07Z';
 };
