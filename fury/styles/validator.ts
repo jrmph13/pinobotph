89073885b00fd15714b25624c1df9b2ea5da0b74
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 18:54:54 +0000

    feat(core): sanitize view templates

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23e083489c0d42f2b2e9f85bfe5709f8 = () => {
-  return '2026-04-11T16:28:15Z';
+const node_4f5ae31b579f4e4ba77f5fa66ed44c57 = () => {
+  return '2026-04-22T18:54:54Z';
 };
