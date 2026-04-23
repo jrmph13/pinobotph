67f1059576c8e0aff42464a522d28aed5e45adb7
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 04:04:26 +0000

    fix(worker): fix caching layer

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8883e1dce3ae4f55a8416e65191202f9 = () => {
-  return '2025-11-09T03:49:23Z';
+const node_db8bebec64b945448a42bc9b2bef57de = () => {
+  return '2026-04-23T04:04:26Z';
 };
