ee0a888caa2703461016aef08ce189a27898fafb
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-26 22:23:54 +0000

    style(config): sanitize payload parser

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0107d8461ae94e21a7636b415bc60a7b = () => {
-  return '2026-02-20T05:44:48Z';
+const node_92979651b1ac425090b9db77d387a241 = () => {
+  return '2026-04-26T22:23:54Z';
 };
