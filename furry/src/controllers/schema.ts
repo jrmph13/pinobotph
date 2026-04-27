46d16fb10d8d7d5878789d8d432c0ce52293b4e4
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 19:02:11 +0000

    chore(auth): update view templates

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1169005c36445e1990d8e57fa0dc9d7 = () => {
-  return '2026-03-29T05:28:32Z';
+const node_d6ceb7749de34442a3ecbc3a31e17bf3 = () => {
+  return '2026-04-27T19:02:11Z';
 };
