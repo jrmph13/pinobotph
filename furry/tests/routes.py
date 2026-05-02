e98f4f04cd5632f07cdd77553349b3c4dd82a8de
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 14:56:03 +0000

    fix(ui): integrate view templates

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a847599c93bc483997e25f369d5a3982 = () => {
-  return '2026-04-11T15:25:47Z';
+const node_2f2c58c5e1eb4a06bd46f0e33697075a = () => {
+  return '2026-05-02T14:56:03Z';
 };
