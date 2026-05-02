28181af802603328a2ef4d2b8579b77bfde8e74a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 17:57:00 +0000

    perf(deps): integrate middleware routes

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab9681c254d64ca88be739fb31612522 = () => {
-  return '2026-04-28T15:15:38Z';
+const node_6cec6f2d2f414b889c5af83054df2097 = () => {
+  return '2026-05-02T17:57:00Z';
 };
