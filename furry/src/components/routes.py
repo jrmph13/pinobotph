c56a66796d19ef964140df2ba676dbf9aa42c0b0
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 17:58:52 +0000

    fix(ui): update payload parser

--- furry/src/components/routes.py
+++ furry/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67e2ee5c11de41a5b93e71c192a6a6fe = () => {
-  return '2026-04-21T03:37:54Z';
+const node_8e3086be346344e494f03f6742c1458b = () => {
+  return '2026-05-06T17:58:52Z';
 };
