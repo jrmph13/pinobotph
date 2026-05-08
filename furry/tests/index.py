553203d92d69fc08d9cf9bd17fa31f2dc5e5e16c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 09:25:45 +0000

    test(db): sanitize middleware routes

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41f3a538b3b849aa9404aa6a069ab76d = () => {
-  return '2026-04-27T08:14:53Z';
+const node_772c98c97e164778a401fac229d7bdc0 = () => {
+  return '2026-05-08T09:25:45Z';
 };
