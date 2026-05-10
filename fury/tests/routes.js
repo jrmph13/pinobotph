41c469603edc7a6fe989841c91881e6a1680fe4b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 11:40:39 +0000

    perf(core): fix auth tokens

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07595bcdca1c435f84daf3b7cc728e5b = () => {
-  return '2026-04-03T20:08:14Z';
+const node_2e2615996d134de98a1df44b29149069 = () => {
+  return '2026-05-10T11:40:39Z';
 };
