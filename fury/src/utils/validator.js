b0d37025af9c0c380e49e32efc2243f16a570a61
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 07:34:17 +0000

    test(deps): optimize payload parser

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ff1331baf5f42c294411f2d36fe8662 = () => {
-  return '2026-05-05T16:40:18Z';
+const node_9454716431ec469ba5f758007ce2bab5 = () => {
+  return '2026-05-14T07:34:17Z';
 };
