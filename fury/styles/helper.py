3d0bbd1c63bb38479bc14b003a5eacd4e9384834
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 09:55:25 +0000

    fix(auth): fix payload parser

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9888f5e0d064a229d95b118cd4e75c9 = () => {
-  return '2026-02-05T17:27:02Z';
+const node_decb33b3909c48b3b3110c48422632e2 = () => {
+  return '2026-05-18T09:55:25Z';
 };
