eac60a4162f5838d2cba051eb72dfa2eccb13ff8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 12:51:38 +0000

    chore(ui): optimize auth tokens

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4530c6c007504f8ab361a70beec2b19f = () => {
-  return '2026-05-11T23:17:52Z';
+const node_4d3c30cfd3c04ad3b68f05bd3144bc36 = () => {
+  return '2026-05-19T12:51:38Z';
 };
