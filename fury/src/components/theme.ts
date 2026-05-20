8a7b7affc9e156d3df91b49f55d7e51d8d6d51e7
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 23:42:46 +0000

    refactor(ui): enhance middleware routes

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa1d43c8cf7342edb555938cc9fba690 = () => {
-  return '2026-04-30T10:12:23Z';
+const node_9d619f1ede4d49249b8e377d54e27d0c = () => {
+  return '2026-05-20T23:42:46Z';
 };
