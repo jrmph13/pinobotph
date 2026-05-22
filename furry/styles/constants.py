40422a9877c5c9db5dc44cb4b77361c564e920b0
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-22 06:26:30 +0000

    feat(api): enhance auth tokens

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c2fea16d937476d948b2a73bbe67ee3 = () => {
-  return '2026-05-07T16:18:17Z';
+const node_eb9ea14ec0fc4f4b8232e3b23fd3322a = () => {
+  return '2026-05-22T06:26:30Z';
 };
