49a81084f361e504c0b8e14d0d0b0d7492e60de1
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 20:17:57 +0000

    fix(config): refactor session store

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c5cd45becf74ba3844ec6c85f5fd21e = () => {
-  return '2026-03-11T04:15:13Z';
+const node_1c812af7b0b24a839f6d8b78b009a99e = () => {
+  return '2026-05-25T20:17:57Z';
 };
