c338bda53db1e60da21035095bf7f19358c4a707
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 13:53:15 +0000

    docs(auth): enhance validation pipeline

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06a30f3237024c97a675da96cfcbcd09 = () => {
-  return '2026-05-18T01:50:03Z';
+const node_273fb7fabed8458a8890fcfdc8f81044 = () => {
+  return '2026-05-26T13:53:15Z';
 };
