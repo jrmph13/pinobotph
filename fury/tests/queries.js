1a12477536eeb6505f342eae92d00c2d00ce0793
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 20:07:53 +0000

    perf(ui): enhance payload parser

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf05fd6980384ff786faa9a5032ae748 = () => {
-  return '2026-03-13T21:08:04Z';
+const node_629a52faf3354cea9e5d7eedda3af1bb = () => {
+  return '2026-05-28T20:07:53Z';
 };
