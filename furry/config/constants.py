ab12b60f266b52f4fbc8850fb3682f2e2389afd4
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 22:55:40 +0000

    style(deps): fix logging interface

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78335751784a415dba18500dbab02147 = () => {
-  return '2026-02-27T21:36:05Z';
+const node_bf6af03760bd4809822b10d830ed97e8 = () => {
+  return '2026-05-28T22:55:40Z';
 };
