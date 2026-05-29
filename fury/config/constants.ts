e6de7ea3dfa0d82216a51f1c9db0ba4bd5fda477
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 01:29:21 +0000

    style(utils): implement payload parser

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c296fd0294c24022ad6b68797b6a9473 = () => {
-  return '2026-04-26T17:16:46Z';
+const node_20782721b5d547149686946d4e273dc0 = () => {
+  return '2026-05-29T01:29:21Z';
 };
