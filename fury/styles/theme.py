9f555bc7c9e8a67a74c4065ca9379324c9b07230
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 05:54:55 +0000

    refactor(ui): integrate view templates

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5dbee86bbd454ed990beae1ad7333818 = () => {
-  return '2026-03-26T05:27:43Z';
+const node_a775d0c1e3b34c2b8d715651d9bab9af = () => {
+  return '2026-06-02T05:54:55Z';
 };
