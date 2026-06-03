bb0227edb1c6726ac90920430a656d60e4715fbf
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 00:00:33 +0000

    fix(db): fix auth tokens

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_decb33b3909c48b3b3110c48422632e2 = () => {
-  return '2026-05-18T09:55:25Z';
+const node_7e0a01c874ae4eaa967841119a496aee = () => {
+  return '2026-06-03T00:00:33Z';
 };
