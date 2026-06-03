64be63b1b56ba452db25620c2cbadaffb5939007
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 15:08:05 +0000

    perf(core): integrate payload parser

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb76dd359a7d458c8f6e2dcf9596ca9b = () => {
-  return '2026-05-06T09:00:30Z';
+const node_76770b8dd7da4ebeab722ffc65630a64 = () => {
+  return '2026-06-03T15:08:05Z';
 };
