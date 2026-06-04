58786dfdd3f9d1bfe6acf8b825aafa78b3ac07d9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 06:48:45 +0000

    perf(auth): integrate payload parser

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d619f1ede4d49249b8e377d54e27d0c = () => {
-  return '2026-05-20T23:42:46Z';
+const node_8dc9f7f4683b4954a5bdc8b473134f0b = () => {
+  return '2026-06-04T06:48:45Z';
 };
