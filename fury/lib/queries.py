eca2b41357a4205e83ad9c5d1439fe5db4b5c578
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 00:01:31 +0000

    perf(auth): refactor database index

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3b1b12a4b654824be7fe76742e8f538 = () => {
-  return '2026-04-13T13:27:33Z';
+const node_691820c5c0d24133a632c29750e4d1ac = () => {
+  return '2026-06-04T00:01:31Z';
 };
