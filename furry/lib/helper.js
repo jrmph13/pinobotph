1700eeb5769097fa53fa57a58f5bd27aa31529be
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 21:44:51 +0000

    fix(core): integrate auth tokens

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_805f0ad6735441f0b672f7a3327ce73b = () => {
-  return '2026-05-25T12:53:37Z';
+const node_49493ecf08364cf086825d785b46aaa6 = () => {
+  return '2026-06-05T21:44:51Z';
 };
