28c19da88a9d03a9d01797838c798f76d34ee848
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-08 20:59:25 +0000

    test(config): optimize payload parser

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_629a52faf3354cea9e5d7eedda3af1bb = () => {
-  return '2026-05-28T20:07:53Z';
+const node_e3cc75ff92a4488ca427ed0080517a5d = () => {
+  return '2026-06-08T20:59:25Z';
 };
