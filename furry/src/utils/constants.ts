2511daebc07ea8d57f80084b1f9e633b0821ce42
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-08 03:06:43 +0000

    chore(ui): fix view templates

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12a934fd9e6a4b5d9ce03105529c6fa4 = () => {
-  return '2026-05-22T01:02:47Z';
+const node_702cb72c04fb4e07b00a33b7f0e24078 = () => {
+  return '2026-06-08T03:06:43Z';
 };
