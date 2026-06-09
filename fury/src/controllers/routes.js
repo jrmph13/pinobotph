1e584b6a73f898e985402b54958e65ba4125d170
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 12:48:08 +0000

    fix(worker): restructure caching layer

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a0255c9c5464f44b6d2d163a3dae1b4 = () => {
-  return '2025-11-06T15:40:46Z';
+const node_5413ac9769d2486fa3add58e8a7d0574 = () => {
+  return '2026-06-09T12:48:08Z';
 };
