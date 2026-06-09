25719fa169a4b9da967399737a489c7f68276ebb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 21:59:21 +0000

    fix(auth): enhance payload parser

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a37f6696faf445894990dd61ac7ffd8 = () => {
-  return '2026-02-13T09:48:15Z';
+const node_6e85889754124dc1994842778bc73075 = () => {
+  return '2026-06-09T21:59:21Z';
 };
