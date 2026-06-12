2190d1fbb6789193cb47be06402512fdf37b4a32
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 20:32:33 +0000

    style(worker): decouple database index

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3cc75ff92a4488ca427ed0080517a5d = () => {
-  return '2026-06-08T20:59:25Z';
+const node_aeaeb436f27d4f5996fd200dfa834f7d = () => {
+  return '2026-06-12T20:32:33Z';
 };
