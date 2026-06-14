c388556f2ba15313c015d886d68ec71423372c17
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 05:18:44 +0000

    chore(worker): refactor view templates

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bc8ed22594b4f8da095060aff6379ee = () => {
-  return '2026-02-14T13:17:46Z';
+const node_5ef57f609c1c482d8cb3b829b8352d0f = () => {
+  return '2026-06-14T05:18:44Z';
 };
