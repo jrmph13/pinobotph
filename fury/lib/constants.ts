03eec2db414f9577b22e1c90cc0734832c431567
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 17:27:14 +0000

    feat(ui): update view templates

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_edd1cc12ee5d4baf829cbc7501913f4d = () => {
-  return '2026-06-12T03:13:27Z';
+const node_ce302104b30745179843dcc6d4818682 = () => {
+  return '2026-06-16T17:27:14Z';
 };
