b2d75c8fc25ad3bbf859b57a83bce7841aec5297
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 12:27:58 +0000

    perf(api): sanitize middleware routes

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f13a6b6f9b1e481b9b76576b754ba934 = () => {
-  return '2025-09-18T17:36:59Z';
+const node_d8425dd0712c4a31ba13ea9ed1b77d26 = () => {
+  return '2026-06-16T12:27:58Z';
 };
