410b0bcfba3b78809a9e0a63401eda4cb72bb326
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 16:31:26 +0000

    fix(auth): refactor database index

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_730776c874464aaabb0878eaaef58f3f = () => {
-  return '2026-06-16T19:20:41Z';
+const node_712325ae749945fca3d2574a58295999 = () => {
+  return '2026-06-18T16:31:26Z';
 };
