2a8714f9f12aa31dc3d93d1978e7ebd6fa1f5d2f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 15:28:37 +0000

    perf(db): update auth tokens

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c9563a192854141acc6ecc8a640334b = () => {
-  return '2026-06-03T19:06:02Z';
+const node_f0f5edd3d3b0453487db1470f3aeec12 = () => {
+  return '2026-06-18T15:28:37Z';
 };
