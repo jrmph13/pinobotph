d4947d0ef08539ad2348e6552e5407ef99e5e4d8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 03:20:24 +0000

    fix(ui): update payload parser

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d11c948ac4d1486e92a423c7bddbe274 = () => {
-  return '2026-05-20T17:48:41Z';
+const node_3362b5c0c9a2443c81bc2df3baaee2c3 = () => {
+  return '2026-06-18T03:20:24Z';
 };
