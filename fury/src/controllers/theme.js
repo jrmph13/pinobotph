41ef57e15a041e1d676bc62e8059a8e3351f5001
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 14:38:31 +0000

    chore(deps): update payload parser

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d6b0442f8104f1098b6664ad85ac527 = () => {
-  return '2026-04-23T08:29:47Z';
+const node_f21c96400c454ebda5f2fb4994b9591c = () => {
+  return '2026-06-24T14:38:31Z';
 };
