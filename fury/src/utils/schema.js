672c83ce7a7cc9eb060664c6fe5fa2b25885e00f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 16:35:37 +0000

    fix(utils): enhance session store

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c21624f4eea454e9944719bca0c70e8 = () => {
-  return '2026-06-06T06:27:15Z';
+const node_572cebfe34a448a595554b1dd55e1743 = () => {
+  return '2026-06-25T16:35:37Z';
 };
