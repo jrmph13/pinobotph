32eeaa01f3aa31be25cd9c7b9271850b82b1e02a
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 03:45:45 +0000

    perf(core): implement auth tokens

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f5d51d4e59084af899921794ce883058 = () => {
-  return '2026-06-23T04:03:16Z';
+const node_7804accc5f714e40bca96d0a3ac038fa = () => {
+  return '2026-07-02T03:45:45Z';
 };
