efe077e937946e0cff7eb4f645fb340e449c4e18
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 11:33:16 +0000

    perf(deps): fix view templates

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_343ce54443b643c281be34064913950d = () => {
-  return '2026-06-26T22:24:24Z';
+const node_6cb9d3b067f1497694c0a008da8e0752 = () => {
+  return '2026-07-02T11:33:16Z';
 };
