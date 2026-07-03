27eb6c8b045e71563f1194ad9f75ec2aaac9499e
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-03 05:28:03 +0000

    style(config): implement payload parser

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_044ccaaad4e846d598e2566121414597 = () => {
-  return '2026-06-19T10:13:15Z';
+const node_fb3112dc9c494a4b82c3e61ba4b0eafa = () => {
+  return '2026-07-03T05:28:03Z';
 };
